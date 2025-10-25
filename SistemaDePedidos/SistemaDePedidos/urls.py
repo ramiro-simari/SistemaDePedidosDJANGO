bfc1cee4d86be0d11e7fcff5d6137c5bd882b642
Ramiro <ramirosimari.dev@gmail.com>
2025-10-25 18:22:20 -0300

    agrego remodelado de barra de navegacion

--- SistemaDePedidos/SistemaDePedidos/urls.py
+++ SistemaDePedidos/SistemaDePedidos/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from SistemaPedidosWebApp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,6 @@ urlpatterns = [
     path('pedidos/', include('pedidos.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- SistemaDePedidos/SistemaPedidosWebApp/urls.py
+++ SistemaDePedidos/SistemaPedidosWebApp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from SistemaPedidosWebApp import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="Home"),
 ]
 
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
